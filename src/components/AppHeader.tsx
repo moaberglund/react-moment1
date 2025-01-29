2419f16bb1434eb402f231fea41cca17cd7f4884
moaberglund <moa.21@hotmail.com>
2025-01-29 13:27:55 +0100

    start stage, creating header component

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,16 @@
+import './AppHeader.css';
+
+function AppHeader(props: { sitetitle: string }) {
+  return (
+    <header>
+      <div className='banner'>
+        <h1>{props.sitetitle}</h1>
+      </div>
+      <div className='hero-img'>
+
+      </div>
+    </header>
+  );
+};
+
+export default AppHeader;
