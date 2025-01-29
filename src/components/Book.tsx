417ab6c2b0f60e93b731136c4c4691d020462eea
moaberglund <moa.21@hotmail.com>
2025-01-29 14:07:31 +0100

    starting stage of book component

---
+++ src/components/Book.tsx
@@ -0,0 +1,21 @@
+import './Book.css';
+
+interface BookProps {
+    title: string;
+    author: string;
+    pages: number;
+    read: boolean;
+}
+
+function Book({ title, author, pages, read }: BookProps) {
+    return (
+        <div className="book">
+            <h1>{title}</h1>
+            <p>by {author}</p>
+            <p>{pages} pages</p>
+            <p>{read ? 'Read' : 'Not read'}</p>
+        </div>
+    );
+}
+
+export default Book;
